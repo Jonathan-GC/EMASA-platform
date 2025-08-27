97e6128dc265795e8e1325ff9b50854ea37ea195
JackBlaze132 <ederdmc@hotmail.com>
2025-08-27 14:23:56 -0500

    Add: Implemented new icons plugins based on ionic
    There are several icons scriptsorganized by cathegory and instancete  by
    index.js file and insplaed by plugins.js script
    ---
    NOTE: Changes can be done in the icons system, at this moment is using
    ionicons, but evaluaiton got use iconify

---
+++ Platform_frontend/src/plugins/icons/actionIcons.js
@@ -0,0 +1,34 @@
+// Módulo de iconos de acciones
+import { 
+  eyeOutline,
+  refreshOutline,
+  addOutline,
+  createOutline,
+  trashOutline,
+  downloadOutline,
+  arrowUpOutline, // Usar arrowUpOutline en lugar de uploadOutline
+  copyOutline,
+  saveOutline,
+  printOutline,
+  shareOutline,
+  playOutline,
+  pauseOutline,
+  stopOutline
+} from 'ionicons/icons'
+
+export const actionIcons = {
+  'eye': eyeOutline,
+  'refresh': refreshOutline,
+  'add': addOutline,
+  'edit': createOutline,
+  'delete': trashOutline,
+  'download': downloadOutline,
+  'upload': arrowUpOutline, // Usar arrowUpOutline como alternativa
+  'copy': copyOutline,
+  'save': saveOutline,
+  'print': printOutline,
+  'share': shareOutline,
+  'play': playOutline,
+  'pause': pauseOutline,
+  'stop': stopOutline
+}
---
+++ Platform_frontend/src/plugins/icons/locationIcons.js
@@ -0,0 +1,26 @@
+// Módulo de iconos de ubicación y mapas
+import { 
+  locationOutline,
+  mapOutline,
+  navigateOutline,
+  compassOutline,
+  globeOutline,
+  homeOutline,
+  businessOutline,
+  carOutline,
+  trainOutline,
+  airplaneOutline
+} from 'ionicons/icons'
+
+export const locationIcons = {
+  'location': locationOutline,
+  'map': mapOutline,
+  'navigate': navigateOutline,
+  'compass': compassOutline,
+  'globe': globeOutline,
+  'home': homeOutline,
+  'business': businessOutline,
+  'car': carOutline,
+  'train': trainOutline,
+  'airplane': airplaneOutline
+}
